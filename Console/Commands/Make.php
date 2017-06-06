46cc318665f9565c1ae57406fea0eec22b2330d3
Ammar Faizi <ammarfaizi2@gmail.com>
2017-06-06 11:52:19 +0700

    Improve console

--- Console/Commands/Make.php
+++ Console/Commands/Make.php
@@ -28,6 +28,8 @@ class Make implements Command
     	try {
 			if (count($argument) > 1) {
 				throw new InvalidArgumentException("Invalid command argument !", 400);
+			} else {
+				$this->filename = $argument;
 			}
 		} catch (InvalidArgumentException $e) {
 			print Message::error($e->getMessage(), "InvalidArgumentException", $e->getFile(), $e->getLine());
@@ -38,16 +40,22 @@ class Make implements Command
     }
 
 
-    private function makeFile($template, $name, $function = null)
+    private function makeFile($template, $filename, $function = null)
     {
-    	$file = file_get_contents(filename)
+    	$fileContent = file_get_contents(TEMPLATE_DIR . '/' . $template . '.ice');
+    	if ($function !== null) {
+    		$fileContent = $function($file);
+    	}
+    	file_put_contents($filename, $fileContent);
     }
 
     public function execute()
     {
     	switch ($this->command) {
     		case 'controller':
-    				
+    				$this->makeFile("controller", APP_DIR . '/Controllers/' . $this->filename, function($str){
+    					return str_replace("~~~controller_name~~~", $this->filename, $str);
+    				});
     			break;
     		case 'model':
 
