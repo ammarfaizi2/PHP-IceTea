58a01e7be934f9fc98463b269281812fa6ca6483
Ammar Faizi <ammarfaizi2@gmail.com>
2017-06-06 14:19:29 +0700

    PHP CS Fixer

--- System/Crayner/Database/DB.php
+++ System/Crayner/Database/DB.php
@@ -8,24 +8,25 @@ use System\Crayner\Builder\DatabaseFactory;
 
 class DB extends DatabaseFactory
 {
-	use Singleton;
+    use Singleton;
 
-	public function __construct()
-	{
-		try {
-			$conf = Configer::database();
-			$this->pdo = new \PDO($conf['driver'].":host=".$conf['host'].";dbname=".$conf['dbname'], $conf['user'], $conf['pass'], array(
-					\PDO::ATTR_PERSISTENT => false
-				));
-		} catch (\PDOException $e) {
-					
-		}		
-	}
+    public function __construct()
+    {
+        try {
+            $conf = Configer::database();
+            $this->pdo = new \PDO($conf['driver'].":host=".$conf['host'].";dbname=".$conf['dbname'], $conf['user'], $conf['pass'], array(
+                    \PDO::ATTR_PERSISTENT => false
+                ));
+        } catch (\PDOException $e) {
+        }
+    }
 
-	public static function 
+    public static function insert(string $table, array $value)
+    {
+    }
 
-	public function __destruct()
-	{
-		$this->pdo = null;
-	}
+    public function __destruct()
+    {
+        $this->pdo = null;
+    }
 }
