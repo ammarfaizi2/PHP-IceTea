6fa11d6b484681aa948d8741f3be66f8dfddaaa4
Ammar Faizi <ammarfaizi2@gmail.com>
2017-06-06 16:48:31 +0700

    Improve InputHandler

--- System/Crayner/Input/InputHandler.php
+++ System/Crayner/Input/InputHandler.php
@@ -4,6 +4,7 @@ namespace System\Crayner\Input;
 
 use System\Crayner\Contracts\Input\PostGate;
 use System\Crayner\Contracts\Input\InputBinding;
+use System\Crayner\WhiteHat\Encryption\Teacrypt\Teacrypt;
 
 /**
  * @author Ammar Faizi	<ammarfaizi2@gmail.com>
@@ -38,21 +39,46 @@ class InputHandler implements InputBinding, PostGate
 		$this->phpInput = file_get_contents("php://input");
 	}
 
+	/**
+	 * @return InputHandler
+	 */
 	public function phpInput()
 	{
 		$this->toString = $this->phpInput;
 		return $this;
 	}
 
+	/**
+	 * @param  string	$key
+	 * @return InputHandler
+	 */
 	public function post(string $key)
 	{
 		$this->toString = isset($this->purePost[$key]) ? $this->purePost[$key] : "";
 		return $this;
 	}
 
+	/**
+	 * @param	int	$cat
+	 * @return  InputHandler
+	 */
 	public function escape(int $cat = 0)
 	{
-		$this->toString = addslashes($this->toString);
+		if ($cat === 5) {
+			$this->toString = addcslashes($this->toString, "A..z");
+		} else {
+			$this->toString = addslashes($this->toString);
+		}
+		return $this;
+	}
+
+	/**
+	 * @param  string	$key
+	 * @return InputHandler
+	 */
+	public function encrypt($key)
+	{
+		$this->toString = Teacrypt::encrypt($this->toString, $key);
 		return $this;
 	}
 
