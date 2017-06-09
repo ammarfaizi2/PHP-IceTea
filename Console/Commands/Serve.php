b6dda88fddbae022f51d773ca6745295190f22e2
Ammar Faizi <ammarfaizi2@gmail.com>
2017-06-09 23:53:23 +0700

    Create icetea serve

---
+++ Console/Commands/Serve.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Console\Commands;
+
+use System\Crayner\Contracts\Console\Command;
+
+class Serve implements Command
+{
+	public function prepare($selection, $optional, $command)
+    {
+    }
+
+    public function argument($ar)
+    {
+    }
+
+    public function showResult()
+    {
+    }
+
+	public function execute()
+	{
+        print "IceTea Development Started !\n\n127.0.0.1:8000\n\n";
+		print exec("cd ".realpath(__DIR__."/../../public")." && php -S localhost:8000 --file index.php");
+		die;
+	}
+}
