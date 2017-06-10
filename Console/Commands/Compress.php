38265ee9b14d4abea888566ce516c743e3ad26af
Ammar Faizi <ammarfaizi2@gmail.com>
2017-06-10 20:30:06 +0700

    Create Compress class

---
+++ Console/Commands/Compress.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Console\Commands;
+
+use Console\Color\Message;
+use System\Crayner\PCLZip;
+use System\Crayner\Contracts\Console\Command;
+use Console\Exception\InvalidArgumentException;
+
+
+class Compress implements Command
+{
+	public function __construct()
+	{
+
+	}
+
+	public function prepare()
+	{
+		
+	}
+}
