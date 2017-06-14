8f8432c011738b0a7df1734cac7ac0796b013636
Ammar Faizi <ammarfaizi2@gmail.com>
2017-06-14 11:36:23 +0700

    Fix tgl function (js)

--- public/js/register.js
+++ public/js/register.js
@@ -1,18 +1,20 @@
-window.onload = function(){
-			/**
-			 *	Buat tanggal lahir
-			 */
-			 var i = 1, b = ['','Januari','Februari','Maret','April','Mei','Juni','Juli','Agustus','September','Oktober','November','Desember'],oo='<option>',oc='</option>',so='<select ',sc='</select>',a=so+'required name="tanggal">'+oo+oc;
-			 for(;i<=31;i++){
-			 	a+=oo+i+oc;
-			 }
-			 a+=sc+so+'required name="bulan">';
-			 for(x in b){
-			 	a+=oo+b[x]+oc;
-			 }
-			 a+=sc+so+'required name="tahun">'+oo+oc;
-			 for(i=<?php print date("Y"); ?>;i>=1960;i--){
-			 	a+=oo+i+oc;
-			 }
-			 document.getElementById('tgl').innerHTML = a+'</select>';
-		}
+
+
+/**
+ *	Buat tanggal lahir
+ */
+function tgl(y){
+	var i = 1, b = ['','Januari','Februari','Maret','April','Mei','Juni','Juli','Agustus','September','Oktober','November','Desember'],oo='<option>',oc='</option>',so='<select ',sc='</select>',a=so+'required name="tanggal">'+oo+oc;
+	for(;i<=31;i++){
+		a+=oo+i+oc;
+	}
+	a+=sc+so+'required name="bulan">';
+	for(x in b){
+		a+=oo+b[x]+oc;
+	}
+	a+=sc+so+'required name="tahun">'+oo+oc;
+	for(i=y;i>=1960;i--){
+		a+=oo+i+oc;
+	}
+	document.getElementById('tgl').innerHTML = a+'</select>';
+}
