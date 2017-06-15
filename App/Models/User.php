204412e1488885209727156efcdb5674c0a52dd5
Ammar Faizi <ammarfaizi2@gmail.com>
2017-06-15 13:36:08 +0700

    Create User model

---
+++ App/Models/User.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use System\Model;
+use System\Crayner\Database\DB;
+
+
+class User extends Model
+{
+	/**
+	 * Constructor.
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}	
+}
