f4df16bfa33a18d04cfd2dfecd822f324c83b129
Ammar Faizi <ammarfaizi2@gmail.com>
2017-06-15 13:22:04 +0700

    PHP CS Fixer

--- App/Controllers/register.php
+++ App/Controllers/register.php
@@ -8,150 +8,150 @@ use App\Models\Register as RegisterModel;
 
 class register extends Controller
 {
-	/**
-	 * Constructor.
-	 */
-	public function __construct()
-	{
-		parent::__construct();
-		$this->load->helper("url");
-		$this->load->helper("assets");
-		$this->load->helper("rstr");
-		$this->load->helper("teacrypt");
-	}
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper("url");
+        $this->load->helper("assets");
+        $this->load->helper("rstr");
+        $this->load->helper("teacrypt");
+    }
 
-	/**
-	 * Default method.
-	 */
-	public function index()
-	{
-		$dyn = (new RegisterModel())->tokenizer();
-		$key = rstr(72);
-		$token = rstr(64);
-		$enc = teacrypt($token, $key);
-		$this->set->cookie("r_tkey", $key, 120)->encrypt("123zxc");
-		$this->set->cookie("r_tkn", $token, 120)->encrypt($key);
-		$this->load->view("register", array("dyn"=>$dyn, "token"=>$token));
-	}
+    /**
+     * Default method.
+     */
+    public function index()
+    {
+        $dyn = (new RegisterModel())->tokenizer();
+        $key = rstr(72);
+        $token = rstr(64);
+        $enc = teacrypt($token, $key);
+        $this->set->cookie("r_tkey", $key, 120)->encrypt("123zxc");
+        $this->set->cookie("r_tkn", $token, 120)->encrypt($key);
+        $this->load->view("register", array("dyn"=>$dyn, "token"=>$token));
+    }
 
-	public function action()
-	{	
-		if ($this->validation()) {
-			$json = array(
-					"status"=>true,
-					"redirect"=>router_url()."/register_success",
-					"alert"=>""
-				);
-		} else {
-			$json = array(
-					"status"=>false,
-					"redirect"=>(isset($this->redirect) ? $this->redirect : ""),
-					"alert"=>$this->alert
-				);
-		}
-		$this->set->header("Content-type","application/json");
-		$a = new RegisterModel();
-		if ($json['status']) {
-			if($a->validDB($this->u)){
-				$a->store();
-				$json = array(
-					"status"=>true,
-					"redirect"=>"/register/success",
-					"alert"=>(isset($a->alert) ? $a->alert : "")
-				);
-			} else {
-				$json = array(
-					"status"=>false,
-					"redirect"=>(isset($a->redirect) ? $a->redirect : ""),
-					"alert"=>$a->alert
-				);
-			}
-		}
-		$a->record($this->u, ($json['status'] ? "true" : "false"));
-		die(json_encode($json));
-	}
+    public function action()
+    {
+        if ($this->validation()) {
+            $json = array(
+                    "status"=>true,
+                    "redirect"=>router_url()."/register_success",
+                    "alert"=>""
+                );
+        } else {
+            $json = array(
+                    "status"=>false,
+                    "redirect"=>(isset($this->redirect) ? $this->redirect : ""),
+                    "alert"=>$this->alert
+                );
+        }
+        $this->set->header("Content-type", "application/json");
+        $a = new RegisterModel();
+        if ($json['status']) {
+            if ($a->validDB($this->u)) {
+                $a->store();
+                $json = array(
+                    "status"=>true,
+                    "redirect"=>"/register/success",
+                    "alert"=>(isset($a->alert) ? $a->alert : "")
+                );
+            } else {
+                $json = array(
+                    "status"=>false,
+                    "redirect"=>(isset($a->redirect) ? $a->redirect : ""),
+                    "alert"=>$a->alert
+                );
+            }
+        }
+        $a->record($this->u, ($json['status'] ? "true" : "false"));
+        die(json_encode($json));
+    }
 
 
-	private $alert;
-	private $u;
-	private $redirect;
-	private function validation()
-	{
-		$input = json_decode($this->input->post("register_data"), true);
-		$this->u = $input;
-		if (!$this->checkRequest() || !is_array($input)) {
-			$this->load->error(404);
-			die;
-		}
-		if (!$this->tokenVerify()) {
-			$this->alert = "Token mismatch!";
-			#$this->redirect = "?ref=token_mismatch&wg=".urlencode(rstr(72));
-			return false;
-		}
-		if (strlen($input['nama'])<4) {
-			$this->alert = "Nama terlalu pendek!";
-			return false;	
-		}
-		if (strlen($input['tempat_lahir'])<5) {
-			$this->alert = "Tempat lahir terlalu pendek!";
-			return false;
-		}
-		$d = explode("-", $input['tanggal_lahir']);
-		if (!checkdate($d[1], $d[2], $d[0])) {
-			$this->alert = "Tanggal lahir tidak valid!";
-			return false;
-		}
-		if (strlen($input['phone'])<10 || preg_match("#[^0-9\+]#", $input['phone'])) {
-			$this->alert = "Nomor hp tidak valid!~";
-			return false;
-		}
-		if (!filter_var($input['email'], FILTER_VALIDATE_EMAIL)) {
-			$this->alert = "E-Mail tidak valid!";
-			return false;
-		}
-		if (strlen($input['alamat'])<10) {
-			$this->alert = "Alamat kurang lengkap!";
-			return false;
-		}
-		if (preg_match("#[^0-9a-zA-Z\_\.]#", $input['username'])) {
-			$this->alert = "Username hanya boleh mengandung karakter a-zA-Z0-9 . _";
-			return false;
-		}
-		if (strlen($input['username'])<4) {
-			$this->alert = "Username terlalu pendek!\nMinimal 4 karakter.";
-			return false;
-		}
-		if (strlen($input['username'])>20) {
-			$this->alert = "Username terlalu panjang!\nMaksimal 20 karakter.";
-			return false;
-		}
-		if (strlen($input['password'])<6) {
-			$this->alert = "Password terlalu pendek!\nMinimal 6 karakter.";
-			return false;
-		}
-		if (strlen($input['password'])>64) {
-			$this->alert = "Password terlalu panjang!\nMaksimal 64 karakter.";
-			return false;
-		}
-		if ($input['password']!==$input['cpassword']) {
-			$this->alert = "Konfirmasi Password tidak sama!";
-			return false;
-		}
-		return true;
-	}
+    private $alert;
+    private $u;
+    private $redirect;
+    private function validation()
+    {
+        $input = json_decode($this->input->post("register_data"), true);
+        $this->u = $input;
+        if (!$this->checkRequest() || !is_array($input)) {
+            $this->load->error(404);
+            die;
+        }
+        if (!$this->tokenVerify()) {
+            $this->alert = "Token mismatch!";
+            #$this->redirect = "?ref=token_mismatch&wg=".urlencode(rstr(72));
+            return false;
+        }
+        if (strlen($input['nama'])<4) {
+            $this->alert = "Nama terlalu pendek!";
+            return false;
+        }
+        if (strlen($input['tempat_lahir'])<5) {
+            $this->alert = "Tempat lahir terlalu pendek!";
+            return false;
+        }
+        $d = explode("-", $input['tanggal_lahir']);
+        if (!checkdate($d[1], $d[2], $d[0])) {
+            $this->alert = "Tanggal lahir tidak valid!";
+            return false;
+        }
+        if (strlen($input['phone'])<10 || preg_match("#[^0-9\+]#", $input['phone'])) {
+            $this->alert = "Nomor hp tidak valid!~";
+            return false;
+        }
+        if (!filter_var($input['email'], FILTER_VALIDATE_EMAIL)) {
+            $this->alert = "E-Mail tidak valid!";
+            return false;
+        }
+        if (strlen($input['alamat'])<10) {
+            $this->alert = "Alamat kurang lengkap!";
+            return false;
+        }
+        if (preg_match("#[^0-9a-zA-Z\_\.]#", $input['username'])) {
+            $this->alert = "Username hanya boleh mengandung karakter a-zA-Z0-9 . _";
+            return false;
+        }
+        if (strlen($input['username'])<4) {
+            $this->alert = "Username terlalu pendek!\nMinimal 4 karakter.";
+            return false;
+        }
+        if (strlen($input['username'])>20) {
+            $this->alert = "Username terlalu panjang!\nMaksimal 20 karakter.";
+            return false;
+        }
+        if (strlen($input['password'])<6) {
+            $this->alert = "Password terlalu pendek!\nMinimal 6 karakter.";
+            return false;
+        }
+        if (strlen($input['password'])>64) {
+            $this->alert = "Password terlalu panjang!\nMaksimal 64 karakter.";
+            return false;
+        }
+        if ($input['password']!==$input['cpassword']) {
+            $this->alert = "Konfirmasi Password tidak sama!";
+            return false;
+        }
+        return true;
+    }
 
-	private function checkRequest()
+    private function checkRequest()
     {
         return isset($_SERVER['HTTP_X_REQUESTED_WITH']) and $_SERVER['HTTP_X_REQUESTED_WITH'] === "XMLHttpRequest";
     }
 
     private function tokenVerify()
     {
-    	$decrypted_token = $this->get->cookie("r_tkn")->decrypt($this->get->cookie("r_tkey")->decrypt("123zxc"))->__toString();
-    	return $this->u['hash']===sha1($decrypted_token."aaa") && $decrypted_token===$this->u['token'];
+        $decrypted_token = $this->get->cookie("r_tkn")->decrypt($this->get->cookie("r_tkey")->decrypt("123zxc"))->__toString();
+        return $this->u['hash']===sha1($decrypted_token."aaa") && $decrypted_token===$this->u['token'];
     }
 
-	/*private function tanggal_lahir()
+    /*private function tanggal_lahir()
     {
         $a = '<select required name="tanggal"><option></option>';
         if (isset($this->saved_post['tanggal'])) {
@@ -176,7 +176,7 @@ class register extends Controller
                 $a.='<option value="'.($i++).'">'.$val.'</option>';
             }
         }
-        
+
         $a.='</select>';
         $a.= '<select required name="tahun"><option></option>';
         if (isset($this->saved_post['tahun'])) {
