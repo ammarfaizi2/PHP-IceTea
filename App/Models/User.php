632a034c18105a2efa914652defb04b5c38c497a
Ammar Faizi <ammarfaizi2@gmail.com>
2017-06-15 13:51:24 +0700

    Create getUserInfo method

--- App/Models/User.php
+++ App/Models/User.php
@@ -15,4 +15,15 @@ class User extends Model
 	{
 		parent::__construct();
 	}	
+
+	public function getUserInfo($userid)
+	{
+		$pdo = DB::pdoInstance();
+		$st = $pdo->prepare("SELECT `a`.`email`,`a`.`username`,`a`.`ukey`,`a`.`authority`,`a`.`status`,`a`.`verified`,`b`.`nama`,`b`.`tempat_lahir`,`b`.`tanggal_lahir`,`b`.`alamat`,`b`.`phone`,`b`.`last_login`,`b`.`hid` FROM `account_data` AS `a` INNER JOIN `account_info` AS `B` ON `a`.`userid`=`b`.`userid` WHERE `a`.`userid`=:userid LIMIT 1;");
+		$st->execute([":userid"=>$userid]);
+		if ($err = $st->errorInfo()) {
+			var_dump($err);
+		}
+		return $st->fetch(\PDO::FETCH_ASSOC);
+	}
 }
