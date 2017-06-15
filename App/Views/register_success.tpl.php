626d6077a0053b4776628aa9f0d644ff09abcc76
Ammar Faizi <ammarfaizi2@gmail.com>
2017-06-15 16:09:38 +0700

    Update register success page

--- App/Views/register_success.tpl.php
+++ App/Views/register_success.tpl.php
@@ -5,5 +5,10 @@
 </head>
 <body>
 Berhasil mendaftar !
+<br><br>
+Silahkan cek email untuk melakukan verifikasi akun.<br><br><br>
+<p>Login disini :</p>
+<?php $link = "https://www.crayner.cf/login?ref=reg_success&cr=".rstr(32); ?>
+<a href="<?php print $link; ?>"><?php print $link; ?></a>
 </body>
 </html>
