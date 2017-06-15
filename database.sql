3b558f3c815278f34b52cc183f19defe584454a3
Ammar Faizi <ammarfaizi2@gmail.com>
2017-06-15 12:25:30 +0700

    Improve database

--- database.sql
+++ database.sql
@@ -15,20 +15,20 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `account_data` (
   `userid` varchar(16) NOT NULL,
+  `email` varchar(225) NOT NULL,
   `username` varchar(100) NOT NULL,
   `password` text NOT NULL,
   `ukey` varchar(72) NOT NULL,
   `authority` enum('user','admin','root') NOT NULL DEFAULT 'user',
   `status` enum('active','block') NOT NULL DEFAULT 'active',
   `verified` enum('true','false') NOT NULL DEFAULT 'false',
+  `tokenizer` varchar(144) NOT NULL,
   `created_at` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`userid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
-INSERT INTO `account_data` VALUES
-("9315691134","ammarfaizi2","==wyP1SiKsCtW3c9UDjVWe2rQcAA","MU4wSiHv_opPVWPNA080wmGtH_XH1idqnh7YKV8G0kou__QP8OXpRzPrjf_kHs9315691134","user","active","false","2017-06-14 18:43:02","");
 
 
 
@@ -45,8 +45,6 @@ CREATE TABLE `account_info` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
-INSERT INTO `account_info` VALUES
-("9315691134","Ammar Faizi","Surakarta","1996-12-07","Surakarta, Jawa Tengah","087836832000","","");
 
 
 
@@ -61,7 +59,7 @@ CREATE TABLE `login_history` (
   `login_status` enum('true','false') NOT NULL,
   `created_at` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=85 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
 
 
 
@@ -91,8 +89,6 @@ CREATE TABLE `pending_account` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
-INSERT INTO `pending_account` VALUES
-("9315691134","==w8N9ALKtklR9OaTBHUx0400dxYvMPdosS0SE+Vk98TNS1ducmSUBWlNj8sgDBEI/cLS8QZiZLNyGl4Rl6ZKFjrV869yPNCPrwNmDzdXaHjrS3i0TfCtE/83QZq3vozyGvzKTh8lfxEvKm5S0KjM+0qOoysQbfcPwcEjzC0R6YT0L7yMpKsMmEq1jkjO4yjOR/CRiiywVAA","M0JyowaPNKOdxM1w4HSC95fd1Lr3s5aUT8cvoeEE_Vmx2mK5AfqXlPWldhcXxea5pe_D00q0","2017-06-14 18:43:02");
 
 
 
@@ -106,12 +102,11 @@ CREATE TABLE `register_history` (
   `ip_address` varchar(20) DEFAULT NULL,
   `country_id` varchar(2) DEFAULT NULL,
   `hash` varchar(72) NOT NULL,
-  `try` int(11) NOT NULL DEFAULT '1',
   `status` enum('true','false') NOT NULL DEFAULT 'false',
   `created_at` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=70 DEFAULT CHARSET=latin1;
 
 
 
