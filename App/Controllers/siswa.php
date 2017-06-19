01ac9c0754f6452a3bc842b1c69de9cd861ad572
Ammar Faizi <ammarfaizi2@gmail.com>
2017-06-19 15:20:35 +0700

    Create siswa controller and model

---
+++ App/Controllers/siswa.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controllers;
+
+use System\Controller;
+
+
+class siswa extends Controller
+{
+	/**
+	 * Constructor.
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	/**
+	 * Default method.
+	 */
+	public function index()
+	{
+	}
+}
