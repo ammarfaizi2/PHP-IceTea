938baebba6110dd50c160cf9e3f997a4bca75273
Ammar Faizi <ammarfaizi2@gmail.com>
2017-06-29 21:26:51 +0700

    Update core

--- System/Crayner/Builder/Database/DatabaseFactory.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace System\Crayner\Builder\Database;
-
-use System\Crayner\Contracts\Database\DatabaseRule;
-
-abstract class DatabaseFactory implements DatabaseRule
-{
-}
--- public/index.php
+++ public/index.php
@@ -1,4 +1,4 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
-
+define("PUBLIC_DIR", __DIR__);
 System\Crayner::run();
