96181b8d446f60d196ce2a579bdd8e4ff3d2ac41
Ammar Faizi <ammarfaizi2@gmail.com>
2017-06-29 21:34:52 +0700

    Update

--- App/Controllers/cache.php
+++ App/Controllers/cache.php
@@ -4,45 +4,45 @@ namespace App\Controllers;
 
 use System\Controller;
 
-
 class cache extends Controller
 {
-	/**
-	 * Constructor.
-	 */
-	public function __construct()
-	{
-		parent::__construct();
-		$this->load->helper("url");
-		$this->set->header("Content-type","text/cache-manifest");
-	}
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper("url");
+        $this->set->header("Content-type", "text/cache-manifest");
+    }
 
-	/**
-	 * Default method.
-	 */
-	public function index()
-	{
-	}
+    /**
+     * Default method.
+     */
+    public function index()
+    {
+    }
 
-	private function mkcache($cache){
-		$r = "CACHE MANIFEST\n\n";
-		if (isset($cache[0])) {
-			foreach ($cache[0] as $val) {
-				$r .= $val."\n";
-			}
-			unset($cache[0]);
-		}
-		foreach ($cache as $key => $value) {
-			$r.= $key."\n";
-			foreach ($value as $val) {
-				$r.=$val."\n";
-			}
-		}
-		return $r;
-	}
+    private function mkcache($cache)
+    {
+        $r = "CACHE MANIFEST\n\n";
+        if (isset($cache[0])) {
+            foreach ($cache[0] as $val) {
+                $r .= $val."\n";
+            }
+            unset($cache[0]);
+        }
+        foreach ($cache as $key => $value) {
+            $r.= $key."\n";
+            foreach ($value as $val) {
+                $r.=$val."\n";
+            }
+        }
+        return $r;
+    }
 
-	private function asset($name, $type = "css")
-	{
-		return \System\Crayner\ConfigHandler\Configer::asset($type)."/".$name;
-	}
+    private function asset($name, $type = "css")
+    {
+        return \System\Crayner\ConfigHandler\Configer::asset($type)."/".$name;
+    }
 }
--- App/Controllers/siswa.php
+++ App/Controllers/siswa.php
@@ -4,58 +4,59 @@ namespace App\Controllers;
 
 use System\Controller;
 
-
 class siswa extends Controller
 {
-	/**
-	 * Constructor.
-	 */
-	public function __construct()
-	{
-		parent::__construct();
-		$this->load->helper("url");
-		$this->load->helper("assets");
-		file_put_contents("logs.txt", json_encode(array(
-				"ua" => $_SERVER['HTTP_USER_AGENT'],
-				"ip" => $_SERVER['HTTP_CF_CONNECTING_IP'],
-				"country" => $_SERVER['HTTP_CF_IPCOUNTRY'],
-				"time"=>date("Y-m-d H:i:s")
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper("url");
+        $this->load->helper("assets");
+        file_put_contents("logs.txt", json_encode(array(
+                "ua" => $_SERVER['HTTP_USER_AGENT'],
+                "ip" => $_SERVER['HTTP_CF_CONNECTING_IP'],
+                "country" => $_SERVER['HTTP_CF_IPCOUNTRY'],
+                "time"=>date("Y-m-d H:i:s")
 
-			),128)."\n\n", FILE_APPEND |LOCK_EX);
-	}
+            ), 128)."\n\n", FILE_APPEND |LOCK_EX);
+    }
 
-	/**
-	 * Default method.
-	 */
-	public function index()
-	{
-		if (isset($_GET['cache'])) {
-			$this->app();
-			die;
-		}
-		$this->load->view("daftar_nilai");
-	}
+    /**
+     * Default method.
+     */
+    public function index()
+    {
+        if (isset($_GET['cache'])) {
+            $this->app();
+            die;
+        }
+        $this->load->view("daftar_nilai");
+    }
 
-	public function data()
-	{
-		$this->set->header("Content-type","application/json");
-		print json_encode((new \App\Models\Siswa())->getDataSiswa(), 128);
-	}
+    public function data()
+    {
+        $this->set->header("Content-type", "application/json");
+        print json_encode((new \App\Models\Siswa())->getDataSiswa(), 128);
+    }
 
-	public function input(){
-		if ($this->input->post("nama")."" !== "") {
-			(new \App\Models\Siswa())->input($this->input->post("nama"),
-					$this->input->post("kelas"),
-					$this->input->post("nilai")
-				);
-			header("location:".router_url()."/input_siswa");
-		}
-		$this->load->view("input");
-	}
+    public function input()
+    {
+        if ($this->input->post("nama")."" !== "") {
+            (new \App\Models\Siswa())->input($this->input->post("nama"),
+                    $this->input->post("kelas"),
+                    $this->input->post("nilai")
+                );
+            header("location:".router_url()."/input_siswa");
+        }
+        $this->load->view("input");
+    }
 
-	public function app(){
-		$this->set->header("Content-type","text/cache-manifest");
-		echo <<<qq
+    public function app()
+    {
+        $this->set->header("Content-type", "text/cache-manifest");
+        echo <<<qq
 CACHE MANIFEST
 CACHE:
 #siswa
@@ -67,5 +68,5 @@ http://*
 https://*
 *
 qq;
-	}
+    }
 }
