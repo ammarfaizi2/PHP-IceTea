7967f771a7faf05bad971eb5666f569632713e47
Ammar Faizi <ammarfaizi2@gmail.com>
2017-09-08 18:52:04 +0700

    Add more files and create autoload

---
+++ autoload.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * IceTea Framework Autoload.
+ */
+
+
+
+
+/**
+ * Load config file.
+ */
+require __DIR__."/config.php";
+
+/**
+ * Class loader.
+ */
+function ___load_class($class)
+{
+	$map = [
+		"Models" => "app"
+		"Controllers" => "app"
+	];
+	$a = explode("\\", $class, 2);
+	if (count($a)>1 && isset($map[$a[0]])) {
+		require __DIR__."/".$map[$a[0]]."/".str_replace("\\", "/", $class).".php";
+	} else {
+		require __DIR__."/src/".str_replace("\\", "/", $class).".php";
+	}
+}
+
+
+spl_autoload_register("___load_class");
---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+
+/**
+ * IceTea Framework.
+ */
+
