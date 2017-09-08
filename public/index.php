2496a509aeb4fe662688330a3e6da432a55b3d3f
Ammar Faizi <ammarfaizi2@gmail.com>
2017-09-08 18:41:48 +0700

    Destroy all code

--- public/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-require __DIR__ . '/../vendor/autoload.php';
-define("PUBLIC_DIR", __DIR__);
-System\Crayner::run();
--- public/js/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-define("NOT_FOUND", true);
-require __DIR__ . '/../index.php';
