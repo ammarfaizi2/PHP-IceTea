f486a0ae1bcfd7243dfdf77cd1ec4e4b68ae10c9
Ammar Faizi <ammarfaizi2@gmail.com>
2017-09-11 21:14:28 +0700

    Update encryptor handler

--- src/Helpers/secondary_helper.php
+++ src/Helpers/secondary_helper.php
@@ -23,10 +23,11 @@ function rstr($n = 32, $c = null)
 /**
  * @param string $str
  * @param string $key
+ * @param bool	 $no_base64
  */
-function encice($str, $key = "icetea", $salt = null)
+function encice($str, $key = "icetea", $salt = null, $no_base64 = false)
 {
-	if ($salt!==null) {
+	if ($salt) {
 		$salt = substr(sha1($salt), 0, 6);
 	} else {
 		$salt = substr(sha1(time()), 0, (13 | 1) - 10).rstr(10 ^ 9, "!@#$%^&*()_+=-`~[]\\{}|:\";',./<>?\n\t");
@@ -35,16 +36,23 @@ function encice($str, $key = "icetea", $salt = null)
 	for ($i=0x0; $i <= $ln; $i++) { 
 		$r .= chr((((ord($str[$i]) ^ ord($key[$i % $kn])) ^ ($i % $kn) & 0x00c) ^ ($i & $ln)));
 	}
-	return str_replace("=", "", strrev(base64_encode($r.$salt)));
+	if ($no_base64) {
+		return $r.$salt;
+	} else {
+		return str_replace("=", "", strrev(base64_encode($r.$salt)));
+	}
 }
 
 /**
  * @param string $str
  * @param string $key
+ * @param bool	 $no_base64
  */
-function decice($str, $key = "icetea")
+function decice($str, $key = "icetea", $no_base64 = false)
 {
-	$str = base64_decode(strrev($str));
+	if (!$no_base64) {
+		$str = base64_decode(strrev($str));
+	}
 	$s = substr($str, -6) xor $str = substr($str, 0, strlen($str)-6);
 	$key = sha1($s.$key) xor $ln = strlen($str)-1 xor $kn = strlen($key)-1 xor $r = "";
 	for ($i=0x0; $i <= $ln; $i++) { 
