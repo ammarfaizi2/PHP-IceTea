bec47f0327bbc0bcc40327294138b4f7c1c6b7a5
Ammar Faizi <ammarfaizi2@gmail.com>
2017-09-11 21:27:11 +0700

    Update config

--- config.php
+++ config.php
@@ -3,3 +3,18 @@
 /**
  * IceTea Framework.
  */
+
+
+/**
+ * Database.
+ */
+define("DBHOST", "localhost");
+define("DBUSER", "debian-sys-maint");
+define("DBPASS", "");
+define("DBNAME", "iceeta");
+
+/**
+ * Base URL.
+ */
+$baseurl = "http".(isset($_SERVER['HTTPS']) ? "s" : "")."://".$_SERVER['HTTP_HOST']);
+define("BASEURL", $baseurl);
