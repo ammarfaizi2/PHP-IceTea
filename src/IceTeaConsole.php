f60773c2abc243bf2de702cecbbd6fccca9c5d06
Ammar Faizi <ammarfaizi2@gmail.com>
2017-09-12 17:11:41 +0700

    Prepare IceTea Console

--- src/IceTeaConsole.php
+++ src/IceTeaConsole.php
@@ -1,9 +1,24 @@
 <?php
 
+use Console\Input\ArgvInput;
 
 class IceTeaConsole
 {
     public static function run()
     {
+    	$app = new ArgvInput();
+    	$cmd = "\\Console\\Commands\\".$app->command;
+    	$cmd = new $cmd;
+    	$cmd->input($app->getInput());
+    	if ($cmd->execute()) {
+    		print $cmd->result();
+    		do {
+    			$cmd->input($app->getInput());
+    			$r = $cmd->execute();
+    			$cmd->result();
+    		} while ($r);
+    	} else {
+    		die($cmd->result());
+    	}
     }
 }
