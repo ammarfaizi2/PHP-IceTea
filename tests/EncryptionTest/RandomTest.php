d8b9a4790d9858eb6abf70d705240c9be84e0a3f
Ammar Faizi <ammarfaizi2@gmail.com>
2017-09-16 18:59:17 +0700

    Init phpunit

---
+++ tests/EncryptionTest/RandomTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace tests\EncryptionTest;
+
+use PHPUnit\Framework\TestCase;
+
+class RandomTest extends TestCase
+{
+	public function testBase64Result()
+	{
+		for ($i=0; $i < 1000; $i++) { 
+			$str = rstr(64) xor $key = rstr(32);
+			$a = encice($str, $key);
+			$this->assertTrue(decice($a, $key) === $str);	
+		}
+	}
+}
