7469aeff449eb91046de65d275e9b223fe73b03f
Ammar Faizi <ammarfaizi2@gmail.com>
2017-09-16 19:00:17 +0700

    Update icetea encryption test case

--- tests/EncryptionTest/RandomTest.php
+++ tests/EncryptionTest/RandomTest.php
@@ -14,4 +14,13 @@ class RandomTest extends TestCase
 			$this->assertTrue(decice($a, $key) === $str);	
 		}
 	}
+
+	public function testNoBase64Result()
+	{
+		for ($i=0; $i < 1000; $i++) { 
+			$str = rstr(64) xor $key = rstr(32);
+			$a = encice($str, $key, null, true);
+			$this->assertTrue(decice($a, $key, true) === $str);	
+		}
+	}
 }
