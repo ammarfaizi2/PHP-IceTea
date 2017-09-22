8a0951e3b9cb22275f645e7fa2d5f03cff3b434e
Ammar Faizi <ammarfaizi2@gmail.com>
2017-09-22 18:28:05 +0700

    Update error_handler

--- src/sys.vendor/init/start.php
+++ src/sys.vendor/init/start.php
@@ -2,6 +2,7 @@
 
 function __icetea_start()
 {
+	defined("BASEPATH") or trigger_error("Konstanta BASEPATH belum terdefinisi!");
 	require BASEPATH."/app/Routes/web.php";
 	System\Router::apiFlag();
 	require BASEPATH."/app/Routes/api.php";
