9f581551459c5287af7ff391bf1d34d632260df0
Ammar Faizi <ammarfaizi2@gmail.com>
2017-11-10 18:13:06 +0700

    Fix MainHanlder filename

--- framework/IceTea/Console/MainHandler.php
+++ framework/IceTea/Console/MainHandler.php
@@ -4,11 +4,11 @@ namespace IceTea\Console;
 
 final class MainHandler
 {
+	private $run = [];
 
-
-    public function __construct()
+    public function __construct($run)
     {
-
+    	$this->run = $run;
     }//end __construct()
 
     public function __invoke()
