875f50f7093a1a3cd2d447be8dd0336cf6858915
Ammar Faizi <ammarfaizi2@gmail.com>
2017-11-10 17:35:16 +0700

    Update web.php

--- framework/init/error_handler/web.php
+++ framework/init/error_handler/web.php
@@ -16,25 +16,25 @@ if (defined('__COMPOSER_AUTOLOAD_DIR__')) {
         }
 
         switch ($errno) {
-            case E_USER_ERROR:
-                echo "<b>My ERROR</b> [$errno] $errstr<br />\n";
-                echo "  Fatal error on line $errline in file $errfile";
-                echo ', PHP '.PHP_VERSION.' ('.PHP_OS.")<br />\n";
-                echo "Aborting...<br />\n";
+        case E_USER_ERROR:
+            echo "<b>My ERROR</b> [$errno] $errstr<br />\n";
+            echo "  Fatal error on line $errline in file $errfile";
+            echo ', PHP '.PHP_VERSION.' ('.PHP_OS.")<br />\n";
+            echo "Aborting...<br />\n";
             exit(1);
 
             break;
 
-            case E_USER_WARNING:
-                echo "<b>My WARNING</b> [$errno] $errstr<br />\n";
+        case E_USER_WARNING:
+            echo "<b>My WARNING</b> [$errno] $errstr<br />\n";
             break;
 
-            case E_USER_NOTICE:
-                echo "<b>My NOTICE</b> [$errno] $errstr<br />\n";
+        case E_USER_NOTICE:
+            echo "<b>My NOTICE</b> [$errno] $errstr<br />\n";
             break;
 
-            default:
-                echo "Unknown error type: [$errno] $errstr<br />\n";
+        default:
+            echo "Unknown error type: [$errno] $errstr<br />\n";
             break;
         }//end switch
 
