c3c05173309c067173391745fe5d905d03acc916
Ammar Faizi <ammarfaizi2@gmail.com>
2018-01-12 17:43:37 +0700

    Init more files

---
+++ config/app.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'name' => env('APP_NAME', 'MyIceTea'),
+    'env' => env('APP_ENV', 'production'),
+    'debug' => env('APP_DEBUG', false),
+    'url' => env('APP_URL', 'http://localhost'),
+    'timezone' => 'UTC',
+    'locale' => 'en',
+    'fallback_locale' => 'en',
+    'key' => env('APP_KEY')
+];
---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+
+define("MYICETEA_START", microtime(true));
+
+require __DIR__."/../vendor/autoload.php";
+require __DIR__."/../init/web.php";
+
