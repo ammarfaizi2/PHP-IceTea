19b5a6167c73d7b630f9789bbe9a8dcebe5b58ea
Ammar Faizi <ammarfaizi2@gmail.com>
2018-01-19 12:56:32 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -5,15 +5,14 @@ define("MYICETEA_START", microtime(true));
 require __DIR__."/../vendor/autoload.php";
 $app = require __DIR__."/../init/web.php";
 
-var_dump($app);
-
 $app->capture(
 	[
-		"header" => EsTeh\Foundation\Http\HeaderRequest::capture()
+		"request" => EsTeh\Foundation\Http\Request::capture(),
+		"route"	 => EsTeh\Foundation\Http\Route::capture(),
 	]
 );
 
 $app->sendResponse();
 
-
 $app->terminate();
+
