81bf8b0d5e3cbf2064527bf9d2638c16c1bb499f
Ammar Faizi <ammarfaizi2@gmail.com>
2018-02-18 17:40:39 +0700

    Update index controller

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -9,14 +9,9 @@ class IndexController extends Controller
 {
     public function __construct(Request $request)
     {
-        parent::__construct();
     }
 
     public function index(Request $r)
     {
-    	header("Content-type:text/plain");
-    	$st = DB::table("test")->select("user as pengguna")
-    		->limit(1)->offset(1)->get();
-    	var_dump($st);
     }
 }
